5fa3a18a5b40b71b79a536317aaaaf5335ac6b90
Nkemcha <jasonnjuafac712@gmain.com>
2023-11-22 03:58:17 +0000

    to trac a loss account

---
+++ trac.sh
@@ -0,0 +1,7 @@
+#!/bash/sh
+#this is to create an account 
+#you should be root before exicutting this commands
+
+echo  "please enter your username"
+echo enter password 
+echo  account created successfully
